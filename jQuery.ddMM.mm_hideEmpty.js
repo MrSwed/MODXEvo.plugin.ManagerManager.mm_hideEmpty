f95602de03a3d03dc03f4c4da3d30650261e5c67
Ilyas Ronef <ilyas@DivanDesign.ru>
2022-05-23 11:14:39 +0300

    * `jQuery.ddMM.mm_hideEmpty`: Refactoring.

--- jQuery.ddMM.mm_hideEmpty.js
+++ jQuery.ddMM.mm_hideEmpty.js
@@ -1,6 +1,6 @@
 /**
  * jQuery.ddMM.mm_hideEmpty
- * @version 1.0.1 (2022-05-23)
+ * @version 1.0.2 (2022-05-23)
  * 
  * @copyright 2016–2022
  */
@@ -9,57 +9,71 @@
 $.ddMM.mm_hideEmpty = {
 	/**
 	 * @method hideEmptySections
-	 * @version 1.0.1 (2022-05-23)
+	 * @version 1.0.2 (2022-05-23)
 	 * 
 	 * @desc Hide empty sections.
 	 * 
 	 * @returns {void}
 	 */
 	hideEmptySections: function(){
-		$('.sectionBody[id]:not(:has([name])):not(:has(iframe))').each(function(){
-			var
-				$this = $(this),
-				id =
-					$this
-						.attr('id')
-						.match(/(.+)_[^_]+$/)
-						[1]
-			;
-			
-			//Section header
-			$('#' + id + '_header').hide();
-			
-			//Section body
-			$this.hide();
-		});
+		//Find sections
+		$('.sectionBody[id]')
+			//That has no inputs
+			.not(':has([name])')
+			//And has no iframes
+			.not(':has(iframe)')
+			.each(function(){
+				var
+					$this = $(this),
+					id =
+						$this
+							.attr('id')
+							.match(/(.+)_[^_]+$/)
+							[1]
+				;
+				
+				//Section header
+				$('#' + id + '_header').hide();
+				
+				//Section body
+				$this.hide();
+			})
+		;
 	},
 	
 	/**
 	 * @method hideEmptyTabs
-	 * @version 1.0.1 (2022-05-23)
+	 * @version 1.0.2 (2022-05-23)
 	 * 
 	 * @desc Hide empty tabs.
 	 * 
 	 * @returns {void}
 	 */
 	hideEmptyTabs: function(){
-		$('.tab-pane .tab-page:not(:has([name])):not(:has(iframe))').each(function(){
-			var $this = $(this);
-			
-			//Page
-			$this.hide();
-			
-			//Navigation item
-			$('.tab-pane .tab-row .tab')
-				.eq(
-					$this
-						.get(0)
-						.tabPage
-						.index
-				)
-				.hide()
-			;
-		});
+		//Find tabs
+		$('.tab-pane .tab-page')
+			//That has no inputs
+			.not(':has([name])')
+			//And has no iframes
+			.not(':has(iframe)')
+			.each(function(){
+				var $this = $(this);
+				
+				//Page
+				$this.hide();
+				
+				//Navigation item
+				$('.tab-pane .tab-row .tab')
+					.eq(
+						$this
+							.get(0)
+							.tabPage
+							.index
+					)
+					.hide()
+				;
+			})
+		;
 	}
 };
 })(jQuery);
--- mm_hideempty.php
+++ mm_hideempty.php
@@ -56,7 +56,7 @@ function mm_hideEmpty($params = []){
 			$modx->getConfig('site_url') . 'assets/plugins/managermanager/widgets/mm_hideempty/jQuery.ddMM.mm_hideEmpty.js',
 			'html',
 			'jQuery.ddMM.mm_hideEmpty',
-			'1.0.1'
+			'1.0.2'
 		);
 		
 		$e->output($output);
