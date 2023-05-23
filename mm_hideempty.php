b147e9b9930dac5197a6ffb3aaf542f952d03037
Ilyas Ronef <ilyas@DivanDesign.ru>
2023-05-24 01:14:19 +0400

    * False hidings has been significantly reduced with improved input selector (`:input` is used instead of `[name]`).

--- mm_hideempty.php
+++ mm_hideempty.php
@@ -56,7 +56,7 @@ function mm_hideEmpty($params = []){
 			$modx->getConfig('site_url') . 'assets/plugins/managermanager/widgets/mm_hideempty/jQuery.ddMM.mm_hideEmpty.js',
 			'html',
 			'jQuery.ddMM.mm_hideEmpty',
-			'1.2'
+			'1.2.1'
 		);
 		
 		$e->output($output);
